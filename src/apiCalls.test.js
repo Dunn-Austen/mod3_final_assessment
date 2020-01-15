15f0da6fbee737936d0632269bdfb2b02cfc9b80
Alan Turing <austeyd@gmail.com>
2020-01-15 11:57:54 -0700

    Add tests for postMessage

--- src/apiCalls.test.js
+++ src/apiCalls.test.js
@@ -91,3 +91,58 @@ describe('endConversation', () => {
     expect(endConversation()).rejects.toEqual(Error('fetch failed.'));
   });
 });
+
+describe('postMessage', () => {
+  const mockPrompt = 'I\'m feeling sick';
+  const mockResponse = {message: "This sounds like a difficult situation.  Have you tried reaching out to one of your instructors or Jun about this?"};
+
+  it('should call fetch with the correct url', () => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve(mockResponse)
+      });
+    });
+    const url = 'https://drwatson-api.herokuapp.com/api/message';
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ newMessage: mockPrompt })
+    };
+    postMessage(mockPrompt);
+
+    expect(window.fetch).toHaveBeenCalledWith(url, options);
+  });
+
+  it('should return a new response message if successful', () => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve(mockResponse)
+      });
+    });
+    expect(postMessage(mockPrompt)).resolves.toEqual(mockResponse);
+  });
+
+  it('should return a new and confused response message if prompt is successful but not intelligible', () => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve('I don\'t understand. You can try rephrasing.')
+      });
+    });
+    expect(postMessage('ask;fhasdkjfadsf')).resolves.toEqual('I don\'t understand. You can try rephrasing.');
+  });
+
+  it('should return an error if unsuccessful', () => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: false
+      });
+    });
+
+    expect(postMessage(mockPrompt)).rejects.toEqual(Error('Error posting message.'))
+  });
+});
